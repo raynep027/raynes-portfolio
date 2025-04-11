8607d67cbf53d200aaeaa3c24816aa1938930667
Rayne Perry <perry5795@saskpolytech.ca>
2025-04-11 16:14:57 -0600

    Okay what about now

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/raynes-portfolio/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
